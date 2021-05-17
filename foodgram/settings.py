001c51049f3b1427fd5dc10927c9d8083ea8119e
Denisscore <Denisscore@yandex.ru>
2021-05-17 21:08:35 +0300

    'commit_62'

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -126,10 +126,3 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 VERSION = '1.00'
-
-# REDIS related settings
-REDIS_HOST = 'redis'
-REDIS_PORT = '6379'
-BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
-BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
-CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
